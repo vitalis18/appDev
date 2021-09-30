84701c101f4bc95ed3c85eebff51ed0635e50837
vital <71317456+vitalis18@users.noreply.github.com>
2021-09-30 20:29:26 +0530

    Update DepthFirstSearch.cpp

--- DepthFirstSearch.cpp
+++ DepthFirstSearch.cpp
@@ -30,7 +30,7 @@ void Graph::DFS(int v)
   
     // Recur for all the vertices adjacent
     // to this vertex
-    list<int>::iterator i;
+    vector<int>::iterator i;
     for (i = adj[v].begin(); i != adj[v].end(); ++i)
         if (!visited[*i])
             DFS(*i);
